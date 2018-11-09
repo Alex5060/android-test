17ba92b1a113936f1829405986c19e7fdc6fae0b
Alexandre Eeckhout <etu33833@student.henallux.be>
2018-11-09 15:23:55 +0100

    add current position

--- app/src/main/java/xyz/eeckhout/smartcity/MapsActivity.java
+++ app/src/main/java/xyz/eeckhout/smartcity/MapsActivity.java
@@ -3,8 +3,11 @@ package xyz.eeckhout.smartcity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
@@ -16,6 +19,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,6 +27,8 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.RoundCap;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import xyz.eeckhout.smartcity.DataAccess.ItineraireVeloVilleDAO;
@@ -35,7 +41,7 @@ import xyz.eeckhout.smartcity.Model.VilleNamur.ParkingVelo.ParkingVeloVille;
 import xyz.eeckhout.smartcity.Model.VilleNamur.ParkingVoiture.ParkingAuto;
 import xyz.eeckhout.smartcity.Model.VilleNamur.ParkingVoiture.Record;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnPolylineClickListener  {
 
     private GoogleMap mMap;
     private ArrayList<Marker> markers = new ArrayList<Marker>();
@@ -48,21 +54,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
-                //Permission déjà refusée
-                Toast.makeText(this, "La localisation est nécessaire pour garentir une utilisation optimale !", Toast.LENGTH_LONG).show();
-            } else {
-                //Sinon demander la permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
+        try {
+            File httpCacheDir = new File(this.getCacheDir(), "http");
+            long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
+            HttpResponseCache.install(httpCacheDir, httpCacheSize);
+        } catch (IOException e) {
+            Log.i("Samy", "HTTP response cache installation failed:" + e);
         }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapDefault);
         mapFragment.getMapAsync(this);
+    }
+
+    protected void onStop() {
+        HttpResponseCache cache = HttpResponseCache.getInstalled();
+        if (cache != null) {
+            cache.flush();
+        }
     }
 
     /**
@@ -77,6 +87,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        enableMyLocationIfPermitted();
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
         /* Loading JCDecauxVelos */
         LoadJCDecaux jcDecauxLoading = new LoadJCDecaux();
@@ -97,24 +109,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Set a listener for marker click.
         mMap.setOnMarkerClickListener(this);
-
-        // Instantiates a new Polyline object and adds points to define a rectangle
-
-//        PolylineOptions rectOptions = new PolylineOptions()
-//                .clickable(true)
-//                .add(new LatLng(50.4703316231, 4.8655017399)
-//                , new LatLng(50.4701745972, 4.8652616016)
-//                , new LatLng(50.4701317925,  4.8643163498))
-//                .width(30)
-//                .color(Color.rgb(255, 180, 0))
-//                .startCap(new RoundCap())
-//                .endCap(new RoundCap() )
-//                ;
-//        Polyline polyline = mMap.addPolyline(rectOptions);
-//        polyline.setTag("A");
-
+        mMap.setOnPolylineClickListener(this);
     }
 
+    private void enableMyLocationIfPermitted() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private GoogleMap.OnMyLocationButtonClickListener onMyLocationButtonClickListener =
+            new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    mMap.setMinZoomPreference(15);
+                    return false;
+                }
+            };
+
+    private GoogleMap.OnMyLocationClickListener onMyLocationClickListener =
+            new GoogleMap.OnMyLocationClickListener() {
+                @Override
+                public void onMyLocationClick(@NonNull Location location) {
+
+                    mMap.setMinZoomPreference(12);
+
+                    CircleOptions circleOptions = new CircleOptions();
+                    circleOptions.center(new LatLng(location.getLatitude(),
+                            location.getLongitude()));
+
+                    circleOptions.radius(200);
+                    circleOptions.fillColor(Color.RED);
+                    circleOptions.strokeWidth(6);
+
+                    mMap.addCircle(circleOptions);
+                }
+            };
     public boolean onMarkerClick(final Marker marker) {
         // Check if a click count was set, then display the click count.
          Toast.makeText(this,
@@ -126,6 +163,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // for the default behavior to occur (which is for the camera to move such that the
         // marker is centered and for the marker's info window to open, if it has one).
         return false;
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+        Toast.makeText(this, polyline.getTag().toString(), Toast.LENGTH_LONG).show();
     }
 
     @Override
