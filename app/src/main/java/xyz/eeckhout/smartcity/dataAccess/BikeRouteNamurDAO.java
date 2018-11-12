dc64c48f158a64462c32bd7e279e05575648f8a0
Alexandre Eeckhout <etu33833@student.henallux.be>
2018-11-12 19:02:10 +0100

    Update loading data

--- app/src/main/java/xyz/eeckhout/smartcity/dataAccess/BikeRouteNamurDAO.java
+++ app/src/main/java/xyz/eeckhout/smartcity/dataAccess/BikeRouteNamurDAO.java
@@ -1,5 +1,6 @@
 package xyz.eeckhout.smartcity.dataAccess;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -14,8 +15,26 @@ import xyz.eeckhout.smartcity.model.villeNamur.bikeRoute.BikeRouteNamur;
 
 public class BikeRouteNamurDAO {
     private static final String API_BASE_URL = "https://data.namur.be/api/records/1.0/search/";
-    public BikeRouteNamur getAllItineraireVeloVille() throws Exception
-    {
+    public BikeRouteNamur getAllItineraireVeloVille() throws Exception {
+        URL url = new URL(getApiBaseUrl() + "?dataset=namur-mobilite-itineraires-velo&facet=iti_code_reg&facet=iti_code_eu&facet=iti_communal&facet=iti_code_com&rows=30");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setUseCaches(true);
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder builder = new StringBuilder();
+
+        String stringJSON = "";
+        String line = buffer.readLine();
+        while (line != null) {
+            builder.append(line);
+            line = buffer.readLine();
+        }
+        buffer.close();
+        stringJSON = builder.toString();
+
+        return jsonToItineraireVeloVille(stringJSON);
+    }
+
+    public BikeRouteNamur getItinerairesFromArea(LatLng center, float distance) throws Exception {
         URL url = new URL(getApiBaseUrl() + "?dataset=namur-mobilite-itineraires-velo&facet=iti_code_reg&facet=iti_code_eu&facet=iti_communal&facet=iti_code_com&rows=30");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setUseCaches(true);
@@ -33,6 +52,7 @@ public class BikeRouteNamurDAO {
 
         return jsonToItineraireVeloVille(stringJSON);
     }
+
 
     private BikeRouteNamur jsonToItineraireVeloVille(String stringJSON) throws Exception
     {
